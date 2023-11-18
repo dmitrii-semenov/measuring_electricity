6f95ce55e3dcba6fa1e7180fa4b1e4909b7d2860
evvejiji <240689@vutbr.cz>
2023-11-18 19:15:13 +0100

    Update adc.h

--- measuring_electricity/lib/adc/adc.h
+++ measuring_electricity/lib/adc/adc.h
@@ -1,3 +1,12 @@
 #ifndef ADC_H
 # define ADC_H
+
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+
+/** @brief  ADC conversion from pin
+ *  @param  inp  input port
+ */
+#define ADC_CONVERSION(inp) (((xtalCpu) + 8UL * (baudRate)) / (16UL * (baudRate)) - 1UL)
+
 #endif // ADC_H
